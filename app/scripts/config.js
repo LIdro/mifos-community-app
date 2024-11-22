72804de087b1db3517876b70dcd16f95fbbb2dcc
Lidro <lyflidro@gmail.com>
2024-11-22 02:13:23 +0100

    Configure backend URL to point to fineract-backend.cap.smartlabs.work

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,9 +1,9 @@
-var mifosX = (function (module) {
-    module.ng = {
-        config: {
-            baseApiUrl: window.baseApiUrl || "https://bank.fluidefinance.io",
-            tenantIdentifier: window.tenantIdentifier || "default"
-        }
-    };
-    return module;
-}(mifosX || {}));
+// Configuration file for Fineract backend connection
+(function() {
+    const baseApiUrl = window.baseApiUrl || 'https://fineract-backend.cap.smartlabs.work';
+    const tenantIdentifier = window.tenantIdentifier || 'default';
+
+    window.mifosX = window.mifosX || {};
+    window.mifosX.baseApiUrl = baseApiUrl;
+    window.mifosX.tenantIdentifier = tenantIdentifier;
+})();
